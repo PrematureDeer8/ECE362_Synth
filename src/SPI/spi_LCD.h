49f605d3544d13bc10c3a96cea12c242127aab3d
HanahBradford <bradfoh@purdue.edu>
2025-11-20 15:00:57 -0500

    Implemented SPI LCD display

--- src/SPI/spi_LCD.h
+++ src/SPI/spi_LCD.h
@@ -1,5 +1,18 @@
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
-#include <string.h>
 #include "pico/stdlib.h"
+#include "hardware/spi.h"
+#include "hardware/dma.h"
+#include "hardware/gpio.h"
+#include "pico/stdlib.h"
+
+
+#define SPI_DISP_SCK 10 // spi1
+#define SPI_DISP_CSn 9  // spi1
+#define SPI_DISP_TX 11  // spi1
+
+void init_chardisp_pins();
+void send_spi_cmd(spi_inst_t* spi, uint16_t value);
+void send_spi_data(spi_inst_t* spi, uint16_t value);
+void cd_init();
+void cd_display1(const char *str);
+void cd_display2(const char *str);
+void update_lcd(void);
--- src/main.c
+++ src/main.c
@@ -6,44 +6,53 @@
 
 #include "I2S/I2S.h"
 #include "adc_ctrlr/adc_ctrlr.h"
+#include "SPI/spi_LCD.h"
 
-int main() {
+int main()
+{
     stdio_init_all();
-    //adc controller stuff
+    // adc controller stuff
     initialize_pins();
     init_gpio_irq();
     init_pots();
     init_wavetables();
-    //make a I2S instance for ease of use
+    init_chardisp_pins();
+    cd_init();
+    // make a I2S instance for ease of use
     init_wavegen(5, 7, pio0, true);
     uint32_t index = 0;
     bool toggle_key = 1;
     key_press(0);
     key_press(4);
     key_press(7);
-    while (true) {
-        
-        //dami block (adc controller)
-        if(fx_button_flag){
+    while (true)
+    {
+
+        // dami block (adc controller)
+        if (fx_button_flag)
+        {
             /*if(index >= NUM_NOTES){
                 index = 0;
             }*/
-            if(index == NUM_NOTES){
+            if (index == NUM_NOTES)
+            {
                 toggle_key = !toggle_key;
             }
             index = index % NUM_NOTES;
             // printf("Fx button was triggered!\n");
-    
+
             fx_button_flag = false;
         }
-        if(wavegen_button_flag){
+        if (wavegen_button_flag)
+        {
             printf("Wavegen button was pressed!\n");
             wavegen_button_flag = false;
         }
-        //pots
+        // pots
         update_pots();
-        sleep_ms(5);
+        update_lcd();
+        sleep_ms(50);
+
     }
     free(inst);
 }
-
